e1a6afbb04b9e57ca3ed0c88e110af5a8f10d82a
James <jabme@users.noreply.github.com>
2014-08-25 16:27:17 +1200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'mUwsDMkQp43F61ZsIoKYAIqTEXhavotCXc0n4Fae3LLW2ava', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [2125835668], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'NALCO Water Treatment Data', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
